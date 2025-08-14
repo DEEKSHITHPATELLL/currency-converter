84ddbd20bc63f3d6552ee226e7fcf2543c425b9f
Deekshith <deekshithpatelll560@gmail.com>
2025-08-14 11:13:44 +0530

    updated json files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Currency from "./Components/Currency"
 import "./index.css";
 function App() {
   return (
-    <Router>
-      <Routes>
+    <Routes>
       <Route path='/' element={<Currency/>}/>
-      </Routes>
-      </Router>     
+    </Routes>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // 👈 Add this line (use your repo name)
+  base: '/currency-converter/',  // 👈 Add this line (use your repo name)
 });
